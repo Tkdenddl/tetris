73490b62cc73d00a0d77e6f1ae1fd993f2849f76
carprefer <carprefer@naver.com>
2020-02-08 00:34:23 +0900

    기본 골격 작성

---
+++ tetris/tetris/interface.c
@@ -0,0 +1,56 @@
+#include "interface.h"
+
+
+void gotoxy(int x, int y)
+{
+	COORD Cur;
+	Cur.X = x;
+	Cur.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+}
+void console(int g, int s)
+{
+	char d[30];
+	color(C, B);
+	sprintf(d, "mode con cols=%d lines=%d", g, s);
+	system(d);
+	return;
+}
+void color(int a, int b)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), a | b);
+	return;
+}
+void CursorView(char show)
+{
+	HANDLE hConsole;
+	CONSOLE_CURSOR_INFO ConsoleCursor;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	ConsoleCursor.bVisible = show;
+	ConsoleCursor.dwSize = 1;
+	SetConsoleCursorInfo(hConsole, &ConsoleCursor);
+}
+void box(int g, int s, int gl, int sl)
+{
+	int x, y;
+	gotoxy(g, s);
+	printf("¦£");
+	for (x = 1; x <= gl; x++)
+		printf("¦¡");
+	printf("¦¤ ");
+	for (y = 1; y <= sl; y++)
+	{
+		gotoxy(g, ++s);
+		printf("¦¢");
+		for (x = 1; x <= gl; x++)
+			printf(" ")v
+			printf("¦¢ ");
+	}
+	gotoxy(g, ++s);
+	printf("¦¦");
+	for (x = 1; x <= gl; x++)
+		printf("¦¡");
+	printf("¦¥ ");
+	gotoxy(g + 2, s - sl);
+	return;
+}
---
+++ tetris/tetris/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "header.h"
+
+int main(void)
+{
+
+	game();
+	return 0;
+}
